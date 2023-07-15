b34b9cd22885dbc7fc99afa8a0b482e0b35a60b0
ankita11997 <ankitaparmar288@gmail.com>
2023-07-16 00:24:41 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// let button = document.querySelector("button");
+// let h1 = document.querySelector("h1");
+// let h2 = document.querySelector("h2");
+
+// async function getdata(){
+//     const response = await fetch("https://quotable.io/random");
+//     console.log(response);
+//     const data = await response.json();
+//     h1.innerText = data.content;
+//     h2.innerText = data.author;
+// }
+// button.addEventListener("click",getdata);
+
+// setInterval(()=>{
+//     getdata();
+// },3000);
